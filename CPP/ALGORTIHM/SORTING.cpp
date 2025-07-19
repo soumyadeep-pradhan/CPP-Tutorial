8a2d687f012dcbb607b4bdc4d6b0981b540079f8
Soumyadeep Pradhan <133474598+soumyadeep-pradhan@users.noreply.github.com>
2025-07-19 16:55:08 +0000

    b

--- CPP/ALGORTIHM/SORTING.cpp
+++ CPP/ALGORTIHM/SORTING.cpp
@@ -3,7 +3,10 @@
 #include <algorithm>
 using namespace std;
 bool comparator(pair<int, int> p1, pair<int,int> p2) {
-    if(p1.first < p2.first) return true;
+    if(p1.second < p2.second) return true;
+    if(p1.second > p2.second) return false;
+    else if(p1.first < p2.first) return true; // If second elements are
+      
     else return false; // Sort by first element
 }
 
@@ -47,7 +50,7 @@ int main() {
     }
     std::cout << std::endl; 
 
-    vector<pair<int, int>> vecPairs = {{1, 2}, {2, 1}, {5, 6},{3, 4},{7,1}};
+    vector<pair<int, int>> vecPairs = {{6,2},{1, 2}, {2, 1}, {5, 6},{3, 4},{7,1},{3,1},{3,2}};
     // Sorting vector of pairs based on the first element
     sort(vecPairs.begin(), vecPairs.end());
     //sort on the basis of first element of pair
@@ -56,6 +59,7 @@ int main() {
         std::cout << "(" << val.first << ", " << val.second << ") ";
     }
     std::cout << std::endl;
+    vector<pair<int, int>> vecPairs2 = {{6,2},{1, 2}, {2, 1}, {5, 6},{3, 4},{7,1},{3,1},{3,2}};
     // To sort based on the second element of the pair
     // Using a lambda function as a custom comparator
     // Custom comparator is a lambda function that compares the second element of pairs
@@ -64,14 +68,22 @@ int main() {
     // This will sort the vector of pairs based on the second element of each pair
     // The lambda function captures the pairs by reference and compares their second elements
     // custom comparator is boolean function that returns true if first element is less than second element
-    sort(vecPairs.begin(), vecPairs.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+    sort(vecPairs2.begin(), vecPairs2.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
         return a.second < b.second; // Sort by second element
     });
     std::cout << "Vector of pairs sorted by second element: ";
-    for(auto val : vecPairs) {
+    for(auto val : vecPairs2) {
         std::cout << "(" << val.first << ", " << val.second << ") ";
 
     }
-
+    std::cout << std::endl;
+    vector<pair<int, int>> vecPairs3 = {{6,2},{1, 2}, {2, 1}, {5, 6},{3, 4},{7,1},{3,1}, {3,2}};
+    // Sorting vector of pairs using a custom comparator function
+    sort(vecPairs3.begin(), vecPairs3.end(), comparator);
+    std::cout << "Vector of pairs sorted using custom comparator: ";
+    for(auto val : vecPairs3) {
+        std::cout << "(" << val.first << ", " << val.second << ") ";
+    }
+    std::cout << std::endl;
     return 0;
 }
